d27d0bfd947152ceea06909fc671b02fc1459e2e
Хованский Владимир <vkhovanskiy@gmail.com>
2021-03-12 21:41:23 +0300

    fixed

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -4,12 +4,11 @@ import './br2jsx.css';
 
 export default props => {
     let items = props.text.split(/\<br\s*\/?\>*/);
-    let i = 0;
     return (
-    <div className='br2jsx'>{items.map(x =>
-        <Fragment key={++i}>
+    <div className='br2jsx'>{items.map( (x, index) =>
+        <Fragment key={index}>
             {x}
-            { items.length > i && <br/>}
+            { items.length - 1 > index && <br/>}
         </Fragment>)}
     </div>);
 }
