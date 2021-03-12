e4f493013b6e3195270b30deb2dc3241b7623e43
Хованский Владимир <vkhovanskiy@gmail.com>
2021-03-12 17:19:17 +0300

    added fragment

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -1,22 +1,16 @@
-import React, {Fragment} from 'react';
+import React from 'react';
+import Fragment from 'render-fragment';
 import './br2jsx.css';
 
 export default props => {
-    let bag = [];
     let items = props.text.split(/\<br\s*\/?\>*/);
     let i = 0;
-    items.forEach(element => {
+    return <div className='br2jsx'>{items.map(x => {
         i++;
-        bag.push(element);
-        if (i < items.length)
-            bag.push(<br key={i}/>);
-    });
-    return <div className='br2jsx'>{bag}</div>
-
-    // return <div className='br2jsx'>{props.text.split(/\<br\s*\/?\>*/).map(x => {
-    //     return (<Fragment key={x}>
-    //         {x}<br/>
-    //     </Fragment>)
-    // })}</div>
-
+        return (
+        <Fragment key={x}>
+            {x}
+            { i < items.length && <br/>}
+        </Fragment>)
+    })}</div>
 }
---
+++ br2jsx/components/br2jsx2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './br2jsx.css';
+
+export default props => {
+    let bag = [];
+    let items = props.text.split(/\<br\s*\/?\>*/);
+    let i = 0;
+    items.forEach(element => {
+        i++;
+        bag.push(element);
+        if (i < items.length)
+            bag.push(<br key={i}/>);
+    });
+    return <div className='br2jsx'>{bag}</div>
+}
