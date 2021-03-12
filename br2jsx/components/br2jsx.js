2c9d23e6e54d08e8d7d8a52b7d4f10e9a742111a
Хованский Владимир <vkhovanskiy@gmail.com>
2021-03-12 17:21:47 +0300

    formatting

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -5,12 +5,13 @@ import './br2jsx.css';
 export default props => {
     let items = props.text.split(/\<br\s*\/?\>*/);
     let i = 0;
-    return <div className='br2jsx'>{items.map(x => {
+    return (
+    <div className='br2jsx'>{items.map(x => {
         i++;
         return (
         <Fragment key={x}>
             {x}
             { i < items.length && <br/>}
-        </Fragment>)
-    })}</div>
+        </Fragment>);
+    })}</div>);
 }
