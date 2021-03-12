4a22ada9eecc338b5afa4885155a48e12b790f77
Хованский Владимир <vkhovanskiy@gmail.com>
2021-03-13 01:07:56 +0300

    1

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -3,7 +3,7 @@ import Fragment from 'render-fragment';
 import './br2jsx.css';
 
 export default props => {
-    let items = props.text.split(/\<br\s*\/?\>*/);
+    let items = props.text.split(/\<br\s*\/?\>/);
     return (
     <div className='br2jsx'>{items.map( (x, index) =>
         <Fragment key={index}>
